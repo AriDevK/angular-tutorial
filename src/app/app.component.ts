036ce48ea2f58e10291c59c2530c5c19b3b4295c
AriDevK <AriadneFave@hotmail.com>
2023-09-03 18:56:38 -0500

    Learning SASS and add styles to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,27 @@ export class AppComponent {
   fumoProducts: Fumo[] = [
     {
       name : "Yuyuko",
-      price : 60,
+      price : 80,
       image : "./assets/images/yuyukoFumo.jpg"
     },
     {
       name : "Cirno",
-      price : 60,
+      price : 50,
       image : "./assets/images/cirnoFumo.jpg"
     },
     {
       name : "Reimu",
-      price : 60,
+      price : 65,
       image : "./assets/images/reimuFumo.jpg"
     },
     {
       name : "Marisa",
-      price : 60,
+      price : 65,
       image : "./assets/images/marisaFumo.jpg"
     },
     {
       name : "Scarlet",
-      price : 60,
+      price : 55,
       image : "./assets/images/scarletFumo.jpg"
     }
   ];
