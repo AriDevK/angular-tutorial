9ba7b936c36ccf79c34205eb6884b0df3de13a36
AriDevK <AriadneFave@hotmail.com>
2023-09-03 23:07:17 -0500

    Learn manage init and destroy events using a counter

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { product } from 'src/app/models/product.models';
 
 @Component({
@@ -6,7 +6,18 @@ import { product } from 'src/app/models/product.models';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnDestroy {
+  counter = 0;
+  private counterTimer : number | undefined;
+
+  ngOnInit(): void {
+    this.counterTimer = window.setInterval(() => this.counter++, 1000);
+  }
+
+  ngOnDestroy(): void {
+    window.clearInterval(this.counterTimer)
+  }
+
   @Input() product: product = {
     id: '0',
     name: '-',
